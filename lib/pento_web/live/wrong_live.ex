cc7118660d6340875dbdfac04d3106f9ee614904
Mahir Labib Chowdhury <mahir.labib@pathao.com>
2023-05-05 17:50:38 +0600

    feat: send current time to the frontend

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,14 +2,29 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(params, session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(socket, score: 0, time: time(), message: "Make a guess:")}
+  end
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again"
+    score = socket.assigns.score - 1
+
+    {
+      :noreply,
+      assign(
+        socket,
+        message: message,
+        score: score,
+        time: time()
+      )
+    }
   end
 
   def render(assigns) do
     ~H"""
     <h1>Your score: <%= @score %></h1>
     <h2>
-      <%= @message %>
+      <%= @message %> It's <%= @time %>
     </h2>
     <h2>
       <%= for n <- 1..10 do %>
@@ -19,5 +34,10 @@ defmodule PentoWeb.WrongLive do
       <% end %>
     </h2>
     """
+  end
+
+  def time() do
+    DateTime.utc_now()
+    |> to_string()
   end
 end
